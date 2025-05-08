038ec4a7e111dcb655944e5acf5767fe190b0006
Brandon <68867795+ZipBrandon@users.noreply.github.com>
2025-05-08 18:36:47 -0400

    Format date as clock

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { useEventSource } from "remix-utils/sse/react";
+import { useMemo } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,11 +13,16 @@ export default function Home() {
 
   let data = useEventSource("/sse", { event: "data" });
 
+  const formattedTime = useMemo(() => {
+    if (!data) return "";
+    const date = new Date(data);
+    return date.toLocaleTimeString();
+  }, [data]);
 
   return <div>
-
-    <h1>Home</h1>
-
-<div>{data}</div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">Current Time</h1>
+      <div className="text-6xl font-mono">{formattedTime}</div>
+    </div>
   </div>
 }
